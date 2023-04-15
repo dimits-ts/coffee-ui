1c24d06a696c1d4c125d7bf79484c63c24a88139
Dimitris Tsirbas <manisfatsa@gmail.com>
2023-04-15 16:16:34 +0300

    Add date view functionality

--- app/src/main/java/com/auebds/coffeui/MainMenuActivity.java
+++ app/src/main/java/com/auebds/coffeui/MainMenuActivity.java
@@ -2,7 +2,12 @@ package com.auebds.coffeui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.util.Locale;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        LocalDate today = LocalDate.now();
+        String month = capitalize(today.getMonth().toString());
+        TextView dateView = findViewById(R.id.textDate);
+        dateView.setText(String.format(Locale.ROOT, "%s %d", month, today.getDayOfMonth()));
+    }
+
+    private String capitalize(String str){
+        if(str.isEmpty()) // avoid IndexOutOfBounds exception
+            return str;
+        else
+            return str.substring(0, 1).toUpperCase(Locale.ROOT) +
+                    str.substring(1).toLowerCase(Locale.ROOT);
     }
 }

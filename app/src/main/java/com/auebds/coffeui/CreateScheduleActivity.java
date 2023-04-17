3259f8598b353ebddf525b0a9de9d21d0792cf56
Dimitris Tsirbas <manisfatsa@gmail.com>
2023-04-17 15:53:35 +0300

    Design create schedule screen

---
+++ app/src/main/java/com/auebds/coffeui/CreateScheduleActivity.java
@@ -0,0 +1,14 @@
+package com.auebds.coffeui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_schedule);
+    }
+}

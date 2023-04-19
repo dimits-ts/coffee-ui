478d7168f10ae691e92ba03eac6359bf011012c1
Dimitris Tsirbas <manisfatsa@gmail.com>
2023-04-19 15:39:13 +0300

    Add basic schedule-related entities

---
+++ app/src/main/java/com/auebds/coffeui/entity/DrinkType.java
@@ -0,0 +1,8 @@
+package com.auebds.coffeui.entity;
+
+/**
+ * The available drinks for the coffee machine.
+ */
+public enum DrinkType {
+    ESPRESSO, FRENCH_COFFEE, TEA, HOT_CHOCOLATE
+}
---
+++ app/src/main/java/com/auebds/coffeui/entity/Schedule.java
@@ -0,0 +1,45 @@
+package com.auebds.coffeui.entity;
+
+import java.time.OffsetTime;
+
+/**
+ * A record class representing a saved schedule.
+ * @author Dimitris Tsirmpas
+ */
+public class Schedule {
+
+    private final String name;
+    private final boolean isRepeatable;
+    private final Iterable<Day> days;
+    private final OffsetTime time;
+
+    /**
+     * Create a new schedule.
+     * @param name the name of the schedule
+     * @param isRepeatable whether the schedule must repeat more than once
+     * @param days the days on which the schedule will be active
+     * @param time the time (+ timezone information) when the schedule will activate
+     */
+    public Schedule(String name, boolean isRepeatable, Iterable<Day> days, OffsetTime time) {
+        this.name = name;
+        this.isRepeatable = isRepeatable;
+        this.days = days;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isRepeatable() {
+        return isRepeatable;
+    }
+
+    public Iterable<Day> getDays() {
+        return days;
+    }
+
+    public OffsetTime getTime() {
+        return time;
+    }
+}

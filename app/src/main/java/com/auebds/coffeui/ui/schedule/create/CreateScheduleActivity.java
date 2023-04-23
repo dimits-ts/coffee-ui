235c7ddceb73984e3ce0057eea6e3c23a0f04135
Dimitris Tsirbas <manisfatsa@gmail.com>
2023-04-23 12:59:38 +0300

    Finish implementation of view-activity

--- app/src/main/java/com/auebds/coffeui/ui/schedule/create/CreateScheduleActivity.java
+++ app/src/main/java/com/auebds/coffeui/ui/schedule/create/CreateScheduleActivity.java
@@ -1,38 +1,197 @@
 package com.auebds.coffeui.ui.schedule.create;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
+import com.auebds.coffeui.MainMenuActivity;
 import com.auebds.coffeui.R;
 import com.auebds.coffeui.dao.DebugScheduleDao;
 import com.auebds.coffeui.entity.Day;
+import com.auebds.coffeui.entity.DrinkType;
 
+import java.time.LocalTime;
 import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * The Activity that manages the creation of a schedule.
+ *
+ * @author Dimitris Tsirmpas
+ */
 public class CreateScheduleActivity extends AppCompatActivity {
 
     private final CreateScheduleMvp.CreateSchedulePresenter presenter =
             new CreateSchedulePresenter(new CreateScheduleView(this), new DebugScheduleDao());
 
-    private HashMap<Day, Button> dayButtonHashMap;
+    private Map<Day, Button> dayButtonHashMap;
+    private EditText nameTextArea;
+    private Spinner drinkTypeSpinner;
+    private TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_schedule);
 
-        this.dayButtonHashMap = createDayButtonMap();
+        this.dayButtonHashMap = this.createDayButtonMap();
+
+        this.assignComponents();
+        this.attachRadioButtonListeners();
+        this.assignDayButtonListeners();
+        this.assignBackButtonListener();
+
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(view -> this.presenter.save());
+
+        timePicker.setIs24HourView(true); // because of user feedback
+    }
+
+    String getName() {
+        return nameTextArea.getText().toString();
+    }
+
+    DrinkType getSelectedDrink() {
+        return (DrinkType) drinkTypeSpinner.getSelectedItem();
+    }
+
+    /**
+     * Get the user-selected scheduled time.
+     * @return a LocalTime object containing the time or null if the time is invalid
+     */
+    LocalTime getTime() {
+        if(timePicker.validateInput())
+            return LocalTime.of(timePicker.getHour(), timePicker.getMinute());
+        else
+            return null;
+    }
+
+    /**
+     * Make the button corresponding to a specific day clickable.
+     * @param day the day whose button will be made clickable
+     */
+    void makeClickable(Day day) {
+        Button button = this.dayButtonHashMap.get(day);
+        assert button != null;
+        button.setClickable(true);
+    }
+
+    /**
+     * Make the button corresponding to a specific day un-clickable.
+     * @param day the day whose button will be made un-clickable
+     */
+    void makeUnclickable(Day day) {
+        Button button = this.dayButtonHashMap.get(day);
+        assert button != null;
+        button.setClickable(false);
+    }
+
+    /**
+     * Change the appearance of the button corresponding to a specific day to being selected.
+     * @param day the day whose button will be marked as selected
+     */
+    void markSelected(Day day) {
+        Button button = this.dayButtonHashMap.get(day);
+        assert button != null;
+        button.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),
+                R.color.button_selected));
+    }
+
+    /**
+     * Change the appearance of the button corresponding to a specific day to being un-selected.
+     * @param day the day whose button will be marked as un-selected
+     */
+    void markUnselected(Day day) {
+        Button button = this.dayButtonHashMap.get(day);
+        assert button != null;
+        button.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),
+                R.color.primary_white));
     }
 
     View getRootView() {
         return getWindow().getDecorView().getRootView();
     }
 
-    private HashMap<Day, Button> createDayButtonMap() {
-        return null;
+    void toMenu() {
+        Intent menuIntent = new Intent(CreateScheduleActivity.this, MainMenuActivity.class);
+        startActivity(menuIntent);
+    }
+
+    /**
+     * Make the back button go to the main menu when pressed.
+     */
+    private void assignBackButtonListener() {
+        ImageView backButton = (ImageView) findViewById(R.id.buttonBack);
+        backButton.setOnClickListener(view -> toMenu());
+    }
+
+    /**
+     * Assign values to the internal fields after the layout has been inflated.
+     */
+    private void assignComponents() {
+        this.nameTextArea = (EditText) findViewById(R.id.scheduleNameField);
+        this.drinkTypeSpinner = (Spinner) findViewById(R.id.selectDrinkSpinner);
+        this.timePicker = (TimePicker) findViewById(R.id.timePicker);
+    }
+
+
+    @SuppressLint("ClickableViewAccessibility") //TODO: subclass buttons?
+    private void assignDayButtonListeners() {
+        for(Map.Entry<Day, Button> entry: this.dayButtonHashMap.entrySet()){
+            entry.getValue().setOnTouchListener((view, event) -> {
+                view.performClick();
+                this.presenter.daySelected(entry.getKey());
+                return false; // don't need this event for anything else
+            });
+        }
+    }
+
+    /**
+     * Notify the presenter if any of the choices are selected.
+     */
+    private void attachRadioButtonListeners() {
+        RadioButton onceButton = (RadioButton) findViewById(R.id.buttonRepeatOnce);
+        RadioButton dailyButton = (RadioButton) findViewById(R.id.buttonRepeatDaily);
+        RadioButton weekdayButton = (RadioButton) findViewById(R.id.buttonRepeatWeekday);
+        RadioButton weekendButton = (RadioButton) findViewById(R.id.buttonRepeatWeekend);
+        RadioButton customButton = (RadioButton) findViewById(R.id.buttonRepeatCustom);
+
+        onceButton.setOnClickListener(view -> presenter.groupSelected(RepetitionPeriod.ONCE));
+        dailyButton.setOnClickListener(view -> presenter.groupSelected(RepetitionPeriod.DAILY));
+        weekdayButton.setOnClickListener(view -> presenter.groupSelected(RepetitionPeriod.WEEKDAY));
+        weekendButton.setOnClickListener(view -> presenter.groupSelected(RepetitionPeriod.WEEKEND));
+        customButton.setOnClickListener(view -> presenter.groupSelected(RepetitionPeriod.CUSTOM));
+    }
+
+    /**
+     * Create the internal map mapping days to their toggle buttons.
+     * @return the filled map
+     */
+    private Map<Day, Button> createDayButtonMap() {
+        HashMap<Day, Button> map = new HashMap<>();
+
+        map.put(Day.MONDAY, (Button) findViewById(R.id.buttonMonday));
+        map.put(Day.TUESDAY, (Button) findViewById(R.id.buttonTuesday));
+        map.put(Day.WEDNESDAY, (Button) findViewById(R.id.buttonWendensday));
+        map.put(Day.THURSDAY, (Button) findViewById(R.id.buttonThursday));
+        map.put(Day.FRIDAY, (Button) findViewById(R.id.buttonFriday));
+        map.put(Day.SATURDAY, (Button) findViewById(R.id.buttonSaturday));
+        map.put(Day.SUNDAY, (Button) findViewById(R.id.buttonSunday));
+
+        // check if accessed before initialization
+        assert map.get(Day.MONDAY) != null;
+
+        return map;
     }
 
 }
--- app/src/main/java/com/auebds/coffeui/ui/schedule/create/CreateScheduleView.java
+++ app/src/main/java/com/auebds/coffeui/ui/schedule/create/CreateScheduleView.java
@@ -7,6 +7,9 @@ import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * The concrete implementation of the schedule creation MVP View.
@@ -16,19 +19,53 @@ import java.time.LocalTime;
 class CreateScheduleView implements  CreateScheduleMvp.CreateScheduleView {
     private final static int SNACKBAR_DURATION = BaseTransientBottomBar.LENGTH_SHORT;
     private final CreateScheduleActivity activity;
+    private final HashMap<Day, Boolean> selectedDaysMap;
+    private boolean isRepeatable;
 
     public CreateScheduleView(CreateScheduleActivity activity) {
         this.activity = activity;
+
+        this.selectedDaysMap = new HashMap<>();
+        for(Day day: Day.values()) {
+            this.selectedDaysMap.put(day, false);
+        }
     }
 
     @Override
     public void groupSelected(RepetitionPeriod repetitionPeriod) {
+        switch(repetitionPeriod) {
+            case ONCE:
+            case CUSTOM:
+                allClickable();
+                break;
+            case DAILY:
+                markAllSelected();
+                allUnclickable();
+                break;
+            case WEEKDAY:
+                markAllSelected();
+                markUnselected(Day.SATURDAY);
+                markUnselected(Day.SUNDAY);
+                allUnclickable();
+                break;
+            case WEEKEND:
+                markAllUnselected();
+                markSelected(Day.SATURDAY);
+                markSelected(Day.SUNDAY);
+                allUnclickable();
+                break;
+        }
 
+        this.isRepeatable = repetitionPeriod != RepetitionPeriod.ONCE;
     }
 
     @Override
     public void daySelected(Day day) {
-
+        if(Boolean.TRUE.equals(this.selectedDaysMap.get(day))) {
+            markSelected(day);
+        } else {
+            markUnselected(day);
+        }
     }
 
     @Override
@@ -36,48 +73,89 @@ class CreateScheduleView implements  CreateScheduleMvp.CreateScheduleView {
         //TODO: placeholder error
         String message = String.format("The name %s is already used for another schedule. " +
                 "Please select a different name", name);
-        Snackbar.make(activity.getRootView(), message, SNACKBAR_DURATION).show();
+        Snackbar.make(this.activity.getRootView(), message, SNACKBAR_DURATION).show();
     }
 
     @Override
     public void displayError(String message) {
         //TODO: placeholder error
-        Snackbar.make(activity.getRootView(), message, SNACKBAR_DURATION).show();
+        Snackbar.make(this.activity.getRootView(), message, SNACKBAR_DURATION).show();
     }
 
     @Override
     public void displaySuccess(String message) {
         //TODO: Replace string value
-        Snackbar.make(activity.getRootView(), R.string.app_name, SNACKBAR_DURATION).show();
+        Snackbar.make(this.activity.getRootView(), R.string.app_name, SNACKBAR_DURATION).show();
     }
 
     @Override
     public void toMenu() {
-
+        this.activity.toMenu();
     }
 
     @Override
     public Iterable<Day> getDays() {
-        return null;
+        LinkedList<Day> ls = new LinkedList<>();
+        for(Map.Entry<Day, Boolean> entry: this.selectedDaysMap.entrySet()){
+            if(entry.getValue()) {
+                ls.add(entry.getKey());
+            }
+        }
+        return ls;
     }
 
     @Override
     public LocalTime getTime() {
-        return null;
+        return this.activity.getTime();
     }
 
     @Override
     public String getName() {
-        return null;
+        return this.activity.getName();
     }
 
     @Override
     public boolean isRepeatable() {
-        return false;
+        return this.isRepeatable;
     }
 
     @Override
     public DrinkType getSelectedDrink() {
-        return null;
+        return activity.getSelectedDrink();
     }
+
+    private void markSelected(Day day) {
+        this.activity.markSelected(day);
+        this.selectedDaysMap.put(day, true);
+    }
+
+    private void markUnselected(Day day) {
+        this.activity.markUnselected(day);
+        this.selectedDaysMap.put(day, false);
+    }
+
+    private void allClickable() {
+        for(Day day: Day.values()){
+            this.activity.makeClickable(day);
+        }
+    }
+
+    private void allUnclickable() {
+        for(Day day: Day.values()){
+            this.activity.makeUnclickable(day);
+        }
+    }
+
+    private void markAllSelected() {
+        for(Day day: Day.values()){
+            markSelected(day);
+        }
+    }
+
+    private void markAllUnselected() {
+        for(Day day: Day.values()){
+            markUnselected(day);
+        }
+    }
+
 }

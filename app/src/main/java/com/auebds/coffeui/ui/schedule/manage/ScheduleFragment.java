a04cab19ff638df1953a0141940d9878c2076070
Dimitris Tsirbas <manisfatsa@gmail.com>
2023-05-08 18:15:13 +0300

    Fix formatting issues

--- app/src/main/java/com/auebds/coffeui/ui/schedule/manage/ScheduleFragment.java
+++ app/src/main/java/com/auebds/coffeui/ui/schedule/manage/ScheduleFragment.java
@@ -14,10 +14,13 @@ import android.widget.TextView;
 
 import com.auebds.coffeui.R;
 import com.auebds.coffeui.entity.Day;
+import com.auebds.coffeui.entity.DrinkType;
 import com.auebds.coffeui.entity.Schedule;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Locale;
 
 /**
  * A {@link Fragment} which displays information about a {@link Schedule}.
@@ -28,7 +31,6 @@ public class ScheduleFragment extends Fragment {
 
     // the fragment initialization parameters
     private static final String ARG_SCHEDULE = "schedule";
-    private Schedule schedule;
 
     public ScheduleFragment() {
         // Required empty public constructor
@@ -52,12 +54,6 @@ public class ScheduleFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            this.schedule = (Schedule) getArguments().getSerializable(ARG_SCHEDULE);
-        } else {
-            Log.e("SCHEDULE_FRAGMENT", "null bundle passed to new fragment");
-        }
     }
 
     @Override
@@ -80,30 +76,59 @@ public class ScheduleFragment extends Fragment {
 
     }
 
+    /**
+     * Display the information of a schedule on the created fragment.
+     * @param schedule the schedule to be displayed
+     */
     private void displayInfo(Schedule schedule) {
         TextView typeView = requireView().findViewById(R.id.scheduleDrinkTypeLabel);
-        typeView.setText(schedule.getName());
+        typeView.setText(getLocalizedTypeString(schedule.getType()));
 
         TextView frequencyView = requireView().findViewById(R.id.scheduleFrequencyLabel);
         frequencyView.setText(formatFrequency(schedule.getDays()));
 
         TextView timeView = requireView().findViewById(R.id.scheduleTimeLabel);
-        timeView.setText(schedule.getTime().format(DateTimeFormatter.ofPattern("H : m")));
+        timeView.setText(schedule.getTime().format(DateTimeFormatter.ofPattern("HH : mm")));
 
         TextView activatedView = requireView().findViewById(R.id.scheduleLabelStatus);
         int resCode =  schedule.isActive() ? R.string.status_active: R.string.status_not_active;
         activatedView.setText(getString(resCode));
     }
 
+    /**
+     * Get the localized string name for any drink type from the .xml resource files.
+     * @param type the DrinkType
+     * @return the localized string assigned to this type
+     * @implNote The translation will be retrieved by matching the 1st character in the xml
+     * string array with the DrinkType's ID.
+     */
+    private String getLocalizedTypeString(DrinkType type) {
+        int typeId = type.getId();
+        String[] typeStrings = requireActivity().getResources().getStringArray(R.array.drinks_array);
+        return Arrays.stream(typeStrings)
+                .filter(typeString -> Character.getNumericValue(typeString.charAt(0)) == typeId)
+                .map(typeString -> typeString.substring(2)) // remove number from localized string
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(String.format(Locale.getDefault(),
+                        "Type %s does not match with any provided DrinkType ID. " +
+                                "See @implNote for more details", type)));
+    }
+
+    /**
+     * Get the frequency of the schedule by listing the days it's active.
+     * @param days the days the schedule will be active
+     * @return a string describing the provided days
+     */
     private String formatFrequency(Collection<Day> days) {
+        //TODO: This should be localized
         StringBuilder sb = new StringBuilder();
 
         for(Day day: days) {
-            sb.append(day.toString().substring(0, 2));
-            sb.append(",");
+            sb.append(day.toString().substring(0, 3)); //get first 3 chars of each day
+            sb.append(", ");
         }
 
-        return sb.substring(0, sb.length()-1); // remove last comma
+        return sb.substring(0, sb.length() - 2); // remove last comma
     }
 
 }
